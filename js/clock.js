34eed1b6a9caad81a88834befda84eb2a09550bb
Esra Peker <eesrapekerr@gmail.com>
2023-01-20 21:21:12 +0300

    saat dakika ve saniye 10 dan küçük olduğunda

--- js/clock.js
+++ js/clock.js
@@ -4,11 +4,23 @@ myName.innerHTML= name;
 
 
 // setInterval(onload, 1000);
-setInterval(function(){
+setInterval(function saatiGoster(){
 let tarih = new Date();
-let gun = tarih.getDay()
-let gunler = new Array("Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi")
-let saat = tarih.getHours()+ ':' + tarih.getMinutes()+':'+tarih.getSeconds();
-document.getElementById("myClock").innerHTML = saat + ' ' + gunler[gun];
+let gun = tarih.getDay();
+let gunler = new Array("Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi");
+let saat = tarih.getHours(); //+ ':' + tarih.getMinutes()+':'+tarih.getSeconds();
+let dakika = tarih.getMinutes();
+let saniye = tarih.getSeconds();
+saat =ciftsifir(saat);
+dakika = ciftsifir(dakika);
+saniye=ciftsifir(saniye);
+document.getElementById("myClock").innerHTML = saat + ":" +dakika +":"+ saniye + ' ' + gunler[gun];
 // clearTimeout(saat);
 },1000);
+function ciftsifir(deger){
+    if(deger<10){
+        deger="0"+deger;
+    }
+    return deger;
+}
+window.onload=saatiGoster;
